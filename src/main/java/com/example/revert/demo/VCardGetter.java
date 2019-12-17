49e10d536ee4c29801b5b72241c9818505bdd363
dawido312 <209275@CARBON.CTI.P.LODZ.PL>
2019-12-17 17:04:49 +0100

    Add generating VCard for choosen employee

---
+++ src/main/java/com/example/revert/demo/VCardGetter.java
@@ -0,0 +1,13 @@
+package com.example.revert.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCardGetter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VCardGetter.class, args);
+    }
+
+}
